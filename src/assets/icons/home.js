9597a943fce4fd2c6e6b6a2ba1fcf786fafbebad
Kanybek <kanybektlgn0@gmail.com>
2022-10-03 21:06:48 +0600

    fix: small fixes

--- src/assets/icons/home.js
+++ src/assets/icons/home.js
@@ -2,7 +2,7 @@ export const home = {
     width: 22,
     height: 22,
     fill: 'none',
-    path: {
+    path: [{
         d: 'M20.661 8.28654L18.3299 6.52867V1.66831H14.999V4.0154L10.9998 1L1.33504 8.28654C1.13572 8.47136 1.01566 8.72614 1 8.99751V20.126C1.00098 20.6057 1.3894 20.9945 1.86916 20.996H20.1268C20.6066 20.9945 20.995 20.6057 20.996 20.126V8.9984C20.9806 8.72672 20.8605 8.47158 20.661 8.28654Z',
         fillRule: 'evenodd',
         clipRule: 'evenodd',
@@ -10,5 +10,5 @@ export const home = {
         strokeWidth: 1.5,
         strokeLinecap: 'round',
         strokeLinejoin: 'round'
-    }
+    }],
 }
